228353668a5cd955de1abaccf7e0bab3c5f0db0f
Daniil <Vinland0_0@mail.ru>
2024-05-04 16:15:40 +0500

    base webpack + scss + react

---
+++ src/index.jsx
@@ -0,0 +1,40 @@
+import './main.scss';
+// import Image from "./images/image.png"
+// import logo from "./images/logo.svg"
+import './font/font.scss'
+
+function component(text) {
+    const element = document.createElement('h1');
+    element.textContent = text;
+    return element;
+}
+console.log(1);
+document.body.prepend(component('Проект собран на Webpack'));
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from './components/App.jsx';
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
+)
+
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+)
+
+// function createImg(){
+//     const img = document.createElement("img");
+//     img.src=Image;
+//     document.body.append(img);
+// }
+// createImg();
+
+// function createSvg(){
+//     const img = document.createElement("img");
+//     img.src=logo;
+//     document.body.append(img);
+// }
+// createSvg();
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FilemanagerWebpackPlugin = require("filemanager-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const ImageMinimizerWebpackPlugin = require("image-minimizer-webpack-plugin");
+
+module.exports = {
+    entry: path.join(__dirname, "src", "index.jsx"),
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "index[contenthash:8].js",
+        assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: "asset/resource"
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: path.join('icons', '[name].[contenthash][ext]'),
+                },
+            },
+            {
+                test: /\.(html)$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(woff2|eot|ttf|otf)$/i,
+                type: "asset/resource"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "template.html"),
+            filename: "index.html",
+        }),
+        new FilemanagerWebpackPlugin({
+            events: {
+                onStart: {
+                    delete: ["dist"]
+                },
+                onEnd: {
+                    copy: [
+                        {
+                            source: path.join(__dirname, "src", "static"),
+                            destination: "dist"
+                        }
+                    ]
+                }
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: "main[contenthash:8].css",
+        })
+    ],
+    devServer: {
+        watchFiles: path.join(__dirname, "src"),
+        port: 8000,
+        open: true,
+        hot: true,
+        liveReload: true,
+    },
+    optimization: {
+        minimizer: [
+            new ImageMinimizerWebpackPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerWebpackPlugin.imageminMinify,
+                    options: {
+                        plugins: [
+                            ["gifsicle", { interlaced: true }],
+                            ["jpegtran", { progressive: true }],
+                            ["optipng", { optimizationLevel: 5 }],
+                            ["svgo", { name: "preset-default" }]
+                        ]
+                    }
+                }
+            })
+        ]
+    }
+}
